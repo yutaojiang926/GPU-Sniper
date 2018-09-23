8a8373d72e2dd5aef3141fdc70e5e78d1fc5a54e
yutaojiang926 <yutaohi@gmail.com>
2018-09-23 15:48:39 -0700

    Parses newegg rss
    uses newegg rss feed to spit out into command line what graphics cards (only 1060, 1070 and 1080) are on sale under a certian price and creates a desktop notification for it

--- src/RSS/tests/GPUSniper.java
+++ src/RSS/tests/GPUSniper.java
@@ -8,7 +8,7 @@ import java.awt.TrayIcon.MessageType;
 import java.net.MalformedURLException;
 
 public class GPUSniper {
-    public void displayTray(FeedMessage message) throws AWTException, MalformedURLException {
+    public TrayIcon displayTray() throws AWTException, MalformedURLException {
         //Obtain only one instance of the SystemTray object
         SystemTray tray = SystemTray.getSystemTray();
 
@@ -17,17 +17,19 @@ public class GPUSniper {
         //Alternative (if the icon is on the classpath):
         //Image image = Toolkit.getDefaultToolkit().createImage(getClass().getResource("icon.png"));
 
-        TrayIcon trayIcon = new TrayIcon(image, "Tray Demo");
+        TrayIcon trayIcon = new TrayIcon(image, "GPU Sniper");
         //Let the system resize the image if needed
         trayIcon.setImageAutoSize(true);
-        //Set tooltip text for the tray icon
-        trayIcon.setToolTip(message.getTitle());
         tray.add(trayIcon);
+        return trayIcon;
+    }
 
-        trayIcon.displayMessage("Hello, World", message.getTitle(), MessageType.INFO);
+    public void displayNotif(TrayIcon trayIcon,FeedMessage message){
+        trayIcon.displayMessage( "$"+Double.toString(message.getPrice()), message.getTitle(), MessageType.INFO);
     }
     public static void main(String[] args) throws InterruptedException, MalformedURLException, AWTException {
-
+        GPUSniper td = new GPUSniper();
+        TrayIcon tray = td.displayTray();
         while(true) {
             // creating feed for 1060 on newegg
             RSSFeedParser parser1060 = new RSSFeedParser(
@@ -37,9 +39,8 @@ public class GPUSniper {
             if (feed1060 != null) {
                 System.out.println("scanned 1060");
                 for (FeedMessage message : feed1060.getMessages(150)) {
+                    td.displayNotif(tray,message);
                     System.out.println(message.toString());
-                    GPUSniper td = new GPUSniper();
-                    td.displayTray(message);
                 }
             }
             else{
@@ -48,16 +49,14 @@ public class GPUSniper {
             Thread.sleep(60000);
             // creating feed for 1070 on newegg
             RSSFeedParser parser1070 = new RSSFeedParser(
-                    "https://www.newegg.com/Product/RSS.aspx?Submit=ENE&N=100007709%20601202919&IsNodeId=1&ShowDeactivatedMark=False");
+                    "https://www.newegg.com/Product/RSS.aspx?Submit=ENE&N=100007709%20601201888&IsNodeId=1&ShowDeactivatedMark=False");
             Feed feed1070 = parser1070.readFeed();
 
             if (feed1070 != null) {
                 System.out.println("scanned 1070");
                 for (FeedMessage message : feed1070.getMessages(300)) {
+                    td.displayNotif(tray,message);
                     System.out.println(message.toString());
-                    System.out.println("scanned 1070");
-                    GPUSniper td = new GPUSniper();
-                    td.displayTray(message);
                 }
             }
             else{
@@ -66,15 +65,13 @@ public class GPUSniper {
             Thread.sleep(60000);
             // creating feed for 1080 on newegg
             RSSFeedParser parser1080 = new RSSFeedParser(
-                    "https://www.newegg.com/Product/RSS.aspx?Submit=ENE&N=100007709%20601203901%20601194948&IsNodeId=1&ShowDeactivatedMark=False");
+                    "https://www.newegg.com/Product/RSS.aspx?Submit=ENE&N=100007709%20601201888&IsNodeId=1&ShowDeactivatedMark=False");
             Feed feed1080 = parser1080.readFeed();
             if (feed1080 != null) {
                 System.out.println("scanned 1080");
                 for (FeedMessage message : feed1080.getMessages(400)) {
+                    td.displayNotif(tray,message);
                     System.out.println(message.toString());
-                    System.out.println("scanned 1080");
-                    GPUSniper td = new GPUSniper();
-                    td.displayTray(message);
                 }
             }
             else {
